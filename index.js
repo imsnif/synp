4ec40cc0fc049787314881cd4e4ee8ea7a519cdd
Aram Drevekenin <grimsniffer@gmail.com>
2017-11-10 19:17:07 +0100

    feat: multiple level dependencies

--- index.js
+++ index.js
@@ -53,10 +53,21 @@ function buildYarnTree ({dependencies, packageDir, memo = {}}) {
 }
 
 module.exports = {
-  yarnToNpm (yarnFileString, name, version) {
+  // yarnToNpm (yarnFileString, name, version) {
+  yarnToNpm (packageDir) {
     // TODO: error checking
-    const { object } = lockfile.parse(yarnFileString)
-    const dependencies = Object.keys(object).reduce((memo, packageNameAndVersion) => {
+    const packageJson = fs.readFileSync(path.join(packageDir, 'package.json'))
+    const { name, version } = JSON.parse(packageJson)
+    const yarnLock = fs.readFileSync(path.join(packageDir, 'yarn.lock'), 'utf-8')
+    const { object } = lockfile.parse(yarnLock)
+    const semverDict = Object.keys(object).reduce((memo, packageNameAndVersion) => {
+      const packageData = object[packageNameAndVersion]
+      const resolvedWithChecksum = packageData.resolved
+      const version = resolvedWithChecksum.replace(/^.*-(.+?)\.tgz#.*$/, '$1') // TODO: fix for more complex version strings that npm supports
+      memo[packageNameAndVersion] = version
+      return memo
+    }, {})
+    const { duplicates, deps } = Object.keys(object).reduce((memo, packageNameAndVersion) => {
       const packageData = object[packageNameAndVersion]
       const name = packageNameAndVersion.replace(/@.+?$/, '') // TODO: handle scopes
       const resolvedWithChecksum = packageData.resolved
@@ -64,11 +75,46 @@ module.exports = {
       const resolved = resolvedWithChecksum.replace(/#.+?$/, '')
       const hexChecksum = resolvedWithChecksum.replace(/^.+?#/, '')
       const base64Checksum = Buffer.from(hexChecksum, 'hex').toString('base64')
-      memo[name] = {
-        version,
-        resolved,
-        integrity: `sha1-${base64Checksum}`
+      const packageDeps = packageData.dependencies
+      const requires = packageDeps ? Object.keys(packageDeps).reduce((reqMemo, packageName) => {
+        const packageNameAndVersion = `${packageName}@${packageDeps[packageName]}`
+        const version = semverDict[packageNameAndVersion]
+        reqMemo[packageName] = version
+        return reqMemo
+      }, {}) : undefined
+      if (memo.deps[name] && memo.deps[name].version !== version) {
+        const packageInMemo = Object.assign({}, memo.deps[name])
+        memo.duplicates = memo.duplicates || {}
+        memo.duplicates[name] = packageInMemo // TODO: find package-lock.json logic for this
+        memo.deps[name] = {
+          version,
+          resolved,
+          integrity: `sha1-${base64Checksum}`,
+          requires
+        }
+      } else {
+        memo.deps[name] = {
+          version,
+          resolved,
+          integrity: `sha1-${base64Checksum}`,
+          requires
+        }
       }
+      return memo
+    }, {duplicates: {}, deps: {}})
+    const dependencies = Object.keys(deps).reduce((memo, packageName) => {
+      const entry = deps[packageName]
+      const entryDependencies = entry.requires ? Object.keys(entry.requires).reduce((eMemo, reqPackageName) => {
+        if (duplicates[reqPackageName] && duplicates[reqPackageName].version === entry.requires[reqPackageName]) {
+          eMemo[reqPackageName] = duplicates[reqPackageName]
+        }
+        return eMemo
+      }, {}) : null
+      memo[packageName] = Object.assign(
+        {},
+        entry,
+        entryDependencies && Object.keys(entryDependencies).length > 0 ? {dependencies: entryDependencies} : {}
+      )
       return memo
     }, {})
     return JSON.stringify({
