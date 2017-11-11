61028b809fd8560a4a585e24c2ca1cd5f66b5565
Aram Drevekenin <grimsniffer@gmail.com>
2017-11-11 17:31:09 +0100

    chore: cleanup

--- testing.js
+++
@@ -1,6 +0,0 @@
-const hex = '5faad9c2c07f60dd76770f71cf025b62a63cfd4e'
-const base64 = 'sha1-X6rZwsB/YN12dw9xzwJbYqY8/U4='
-
-console.log(hex === Buffer.from(base64, 'base64').toString('hex'))
-console.log(base64 === Buffer.from(hex, 'hex').toString('base64'))
-console.log(Buffer.from(hex, 'hex').toString('base64'))
