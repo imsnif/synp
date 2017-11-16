5b9c667f7e0068a96fa98daa7f210a8b35b1b8e8
Aram Drevekenin <grimsniffer@gmail.com>
2017-11-16 18:45:52 +0100

    refactor: package-json with node_modules as state

--- index.js
+++ index.js
@@ -6,75 +6,101 @@ const lockfile = require('@yarnpkg/lockfile')
 const nmTree = require('./lib/nmtree')
 const { buildYarnTree, formatYarnTree } = require('./lib/yarn-tools')
 
-function buildSemverDict (object) {
-  return Object.keys(object).reduce((memo, packageNameAndVersion) => {
-    const packageData = object[packageNameAndVersion]
-    const resolvedWithChecksum = packageData.resolved
-    const version = resolvedWithChecksum.replace(/^.*-(.+?)\.tgz#.*$/, '$1') // TODO: fix for more complex version strings that npm supports
-    memo[packageNameAndVersion] = version
-    return memo
+function findPackageInYarnLock (name, version, yarnObject) {
+  const packageKey = Object.keys(yarnObject).find(yPackage => {
+    const yPackageName = yPackage.replace(/^(.+?)@.+?$/, '$1')
+    return yPackageName === name && yarnObject[yPackage].version === version
+  })
+  return yarnObject[packageKey]
+}
+
+function npmStyleResolved (yarnResolved) {
+  const resolved = yarnResolved.replace(/#.*$/, '')
+  const hexChecksum = yarnResolved.replace(/^.*#/, '')
+  const integrity = 'sha1-' + Buffer.from(hexChecksum, 'hex').toString('base64')
+  // TODO: sha512?
+  return { resolved, integrity }
+}
+
+function buildNpmRequires (dependencies, yarnObject) {
+  return Object.keys(dependencies).reduce((npmRequires, depName) => {
+    const depSemver = dependencies[depName]
+    const yarnEntry  = yarnObject[`${depName}@${depSemver}`]
+    if (!yarnEntry) return npmRequires // fsevents, etc.
+    npmRequires[depName] = yarnEntry.version
+    return npmRequires
   }, {})
 }
 
-function buildNpmTree (object, semverDict) {
-  return Object.keys(object).reduce((memo, packageNameAndVersion) => {
-    const packageData = object[packageNameAndVersion]
-    const name = packageNameAndVersion.replace(/@.+?$/, '') // TODO: handle scopes
-    const resolvedWithChecksum = packageData.resolved
-    const version = resolvedWithChecksum.replace(/^.*-(.+?)\.tgz#.*$/, '$1') // TODO: fix for more complex version strings that npm supports
-    const resolved = resolvedWithChecksum.replace(/#.+?$/, '')
-    const hexChecksum = resolvedWithChecksum.replace(/^.+?#/, '')
-    const base64Checksum = Buffer.from(hexChecksum, 'hex').toString('base64')
-    const packageDeps = packageData.dependencies
-    const requires = packageDeps ? Object.keys(packageDeps).reduce((reqMemo, packageName) => {
-      const packageNameAndVersion = `${packageName}@${packageDeps[packageName]}`
-      const version = semverDict[packageNameAndVersion]
-      reqMemo[packageName] = version
-      return reqMemo
-    }, {}) : undefined
-    if (memo.deps[name] && memo.deps[name].version !== version) {
-      const packageInMemo = Object.assign({}, memo.deps[name])
-      memo.duplicates = memo.duplicates || {}
-      memo.duplicates[name] = packageInMemo // TODO: find package-lock.json logic for this
-    }
-    memo.deps[name] = {
-      version,
-      resolved,
-      integrity: `sha1-${base64Checksum}`,
-      requires
-    }
-    return memo
-  }, {duplicates: {}, deps: {}})
+function npmPackageEntry (nodeModulesTree, yarnObject, mPath) {
+  const { name, version, dependencies } = nodeModulesTree[mPath]
+  const yarnEntry = findPackageInYarnLock(name, version, yarnObject)
+  if (!yarnEntry) return null // likely a bundled dependency
+  const yarnResolved = yarnEntry.resolved
+  const { resolved, integrity } = npmStyleResolved(yarnResolved)
+  const entry = {
+    version,
+    resolved,
+    integrity
+  }
+  if (dependencies && Object.keys(dependencies).length > 0) {
+    entry.requires = buildNpmRequires(dependencies, yarnObject)
+  }
+  return entry
 }
 
-function buildNpmDependencies (deps, duplicates) {
-  return Object.keys(deps).reduce((memo, packageName) => {
-    const entry = deps[packageName]
-    const entryDependencies = entry.requires ? Object.keys(entry.requires).reduce((eMemo, reqPackageName) => {
-      if (duplicates[reqPackageName] && duplicates[reqPackageName].version === entry.requires[reqPackageName]) {
-        eMemo[reqPackageName] = duplicates[reqPackageName]
-      }
-      return eMemo
-    }, {}) : null
-    memo[packageName] = Object.assign(
-      {},
-      entry,
-      entryDependencies && Object.keys(entryDependencies).length > 0 ? {dependencies: entryDependencies} : {}
-    )
-    return memo
+function splitPathToModules (relativePath) {
+  return relativePath
+  .filter(p => p !== 'node_modules')
+  .reduce((packages, dirName, index, rPath) => {
+    if (/^@/.test(dirName)) {
+      packages.push(`${dirName}/${rPath[index + 1]}`)
+    } else if (!(rPath[index - 1] && /^@/.test(rPath[index - 1]))) {
+      packages.push(dirName)
+    }
+    return packages
+  }, [])
+}
+
+function buildNpmTree (nodeModulesTree, yarnObject) {
+  const sortedNodeModules = Object.keys(nodeModulesTree)
+  .sort((a, b) => a.split(path.sep).length < b.split(path.sep).length ? -1 : 1)
+  const basePath = sortedNodeModules[0].split(path.sep)
+  return sortedNodeModules.slice(1)
+  .reduce((tree, mPath) => {
+    const relativePath = mPath.split(path.sep).slice(basePath.length)
+    const modulesInPath = splitPathToModules(relativePath)
+    const npmEntry = npmPackageEntry(nodeModulesTree, yarnObject, mPath)
+    if (!npmEntry) return tree
+    if (modulesInPath.length > 1) {
+      const parentPackage = modulesInPath.slice(0, -1).reduce((prev, next) => {
+        if (prev && (prev[next] || (prev.dependencies && prev.dependencies[next]))) {
+          return prev[next] || prev.dependencies[next]
+        } else {
+          return tree
+        }
+      }, tree)
+      const { name } = nodeModulesTree[mPath]
+      parentPackage.dependencies = Object.assign({}, parentPackage.dependencies || {}, {
+        [name]: npmEntry
+      })
+    } else {
+      const { name } = nodeModulesTree[mPath]
+      tree[name] = npmEntry
+    }
+    return tree
   }, {})
 }
 
 module.exports = {
   yarnToNpm (packageDir) {
     // TODO: error checking
+    const yarnLock = fs.readFileSync(path.join(packageDir, 'yarn.lock'), 'utf-8')
+    const yarnObject = lockfile.parse(yarnLock).object
+    const nodeModulesTree = nmTree(packageDir)
+    const dependencies = buildNpmTree(nodeModulesTree, yarnObject)
     const packageJson = fs.readFileSync(path.join(packageDir, 'package.json'))
     const { name, version } = JSON.parse(packageJson)
-    const yarnLock = fs.readFileSync(path.join(packageDir, 'yarn.lock'), 'utf-8')
-    const { object } = lockfile.parse(yarnLock)
-    const semverDict = buildSemverDict(object)
-    const { duplicates, deps } = buildNpmTree(object, semverDict)
-    const dependencies = buildNpmDependencies(deps, duplicates)
     return JSON.stringify({
       name,
       version,
