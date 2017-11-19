bdc67c62bd72ea655eb34449a9c6824899ab1ee5
Aram Drevekenin <grimsniffer@gmail.com>
2017-11-19 19:07:55 +0100

    refactor: test mocks

--- test/cli.spec.js
+++ test/cli.spec.js
@@ -3,48 +3,8 @@
 const fs = require('fs')
 const test = require('tape')
 const sinon = require('sinon')
-const lockfile = require('@yarnpkg/lockfile')
 const synp = require('../')
-
-function mocks ({
-  sandbox,
-  packagePath,
-  yarnPath,
-  pLockPath,
-  yarnExists,
-  pLockExists,
-  nodeModulesExists = true,
-  yarnIsDir = false,
-  nodeModulesIsFile = false
-}) {
-  const fsExists = sandbox.stub(fs, 'existsSync')
-  const statSync = sandbox.stub(fs, 'statSync')
-  sandbox.stub(fs, 'writeFileSync')
-  sandbox.stub(process, 'exit')
-  fsExists.withArgs(`${packagePath}/package-lock.json`).returns(pLockExists)
-  fsExists.withArgs(`${yarnPath}`).returns(yarnExists)
-  fsExists.withArgs(`${packagePath}/node_modules`).returns(nodeModulesExists)
-  statSync.withArgs(`${packagePath}/node_modules`).returns({
-    isDirectory: () => !nodeModulesIsFile
-  })
-  statSync.withArgs(yarnPath).returns({
-    isFile: () => !yarnIsDir
-  })
-  statSync.withArgs(pLockPath).returns({
-    isFile: () => true
-  })
-  sandbox.stub(synp, 'yarnToNpm')
-    .returns('{"mockedResult": "mockedResult"}')
-  sandbox.stub(synp, 'npmToYarn')
-    .returns('{"mockedResult": "mockedResult"}')
-}
-
-function mockProgram (sourceFile) {
-  return {
-    sourceFile,
-    outputHelp: sinon.spy()
-  }
-}
+const { mocks, mockProgram } = require('./mocks')
 
 test('cli tool converts yarn.lock to package-lock.json', async t => {
   t.plan(4)
---
+++ test/mocks.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const fs = require('fs')
+const sinon = require('sinon')
+const synp = require('../')
+
+module.exports = {
+  mocks ({
+    sandbox,
+    packagePath,
+    yarnPath,
+    pLockPath,
+    yarnExists,
+    pLockExists,
+    nodeModulesExists = true,
+    yarnIsDir = false,
+    nodeModulesIsFile = false
+  }) {
+    const fsExists = sandbox.stub(fs, 'existsSync')
+    const statSync = sandbox.stub(fs, 'statSync')
+    sandbox.stub(fs, 'writeFileSync')
+    sandbox.stub(process, 'exit')
+    fsExists.withArgs(`${packagePath}/package-lock.json`).returns(pLockExists)
+    fsExists.withArgs(`${yarnPath}`).returns(yarnExists)
+    fsExists.withArgs(`${packagePath}/node_modules`).returns(nodeModulesExists)
+    statSync.withArgs(`${packagePath}/node_modules`).returns({
+      isDirectory: () => !nodeModulesIsFile
+    })
+    statSync.withArgs(yarnPath).returns({
+      isFile: () => !yarnIsDir
+    })
+    statSync.withArgs(pLockPath).returns({
+      isFile: () => true
+    })
+    sandbox.stub(synp, 'yarnToNpm')
+      .returns('{"mockedResult": "mockedResult"}')
+    sandbox.stub(synp, 'npmToYarn')
+      .returns('{"mockedResult": "mockedResult"}')
+  },
+  mockProgram (sourceFile) {
+    return {
+      sourceFile,
+      outputHelp: sinon.spy()
+    }
+  }
+}
