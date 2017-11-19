9e4e1f0ed7cd2820f2385916b6c198682701334c
Aram Drevekenin <grimsniffer@gmail.com>
2017-11-19 19:18:16 +0100

    refactor: test structure

--- test/integration.spec.js
+++ test/integration.spec.js
@@ -1,53 +1,14 @@
+'use strict'
+
 const test = require('tape')
 const fs = require('fs')
 const lockfile = require('@yarnpkg/lockfile')
 const { yarnToNpm, npmToYarn } = require('../')
-
-function findNonSha1Hashes ({dependencies, memo = []}) {
-  return Object.keys(dependencies).reduce((memo, packageName) => {
-    if (!/^sha1-/.test(dependencies[packageName].integrity)) {
-      memo.push(dependencies[packageName].resolved)
-    }
-    if (dependencies[packageName].dependencies) {
-      findNonSha1Hashes({dependencies: dependencies[packageName].dependencies, memo})
-    }
-    return memo
-  }, memo)
-}
-
-function replaceNonSha1 (yarnObj, resolvedWithNonSha1Hashes) {
-  return Object.assign({}, yarnObj, {object:
-    Object.keys(yarnObj.object).reduce((memo, packageName) => {
-      const entry = yarnObj.object[packageName]
-      const strippedResolved = entry.resolved.replace(/#.+?$/, '')
-      if (resolvedWithNonSha1Hashes.indexOf(strippedResolved) !== -1) {
-        memo[packageName] = Object.assign({}, entry, {resolved: `${strippedResolved}#N/A`})
-      } else {
-        memo[packageName] = entry
-      }
-      return memo
-    }, {})
-  })
-}
-
-function normalizePackageLock (npmObj, resolvedWithNonSha1Hashes) {
-  return Object.assign({}, npmObj, {dependencies:
-    Object.keys(npmObj.dependencies).reduce((memo, packageName) => {
-      const entry = npmObj.dependencies[packageName]
-      if (resolvedWithNonSha1Hashes.indexOf(entry.resolved) !== -1) {
-        memo[packageName] = Object.assign({}, entry, {integrity: `N/A`})
-      } else {
-        memo[packageName] = entry
-      }
-      if (entry.dependencies && Object.keys(entry.dependencies).length > 0) {
-        const { dependencies } = normalizePackageLock(entry, resolvedWithNonSha1Hashes)
-        memo[packageName] = Object.assign({}, memo[packageName], {dependencies})
-      }
-      delete memo[packageName].optional // see caveats in README
-      return memo
-    }, {})
-  })
-}
+const {
+  findNonSha1Hashes,
+  replaceNonSha1,
+  normalizePackageLock
+} = require('./test-utils')
 
 test('translate with one root dependency', async t => {
   t.plan(2)
@@ -87,7 +48,6 @@ test('translate yarn.lock to package-lock with multiple-level dependencies', asy
   try {
     t.plan(1)
     const path = `${__dirname}/fixtures/multiple-level-deps`
-    const yarnLock = fs.readFileSync(`${path}/yarn.lock`, 'utf-8')
     const packageLock = fs.readFileSync(`${path}/package-lock.json`, 'utf-8')
     const res = yarnToNpm(path).replace(/registry.yarnpkg.com/g, 'registry.npmjs.org')
     const resParsed = JSON.parse(res)
@@ -126,7 +86,6 @@ test('translate yarn.lock to package-lock with scopes', async t => {
   try {
     t.plan(1)
     const path = `${__dirname}/fixtures/deps-with-scopes`
-    const yarnLock = fs.readFileSync(`${path}/yarn.lock`, 'utf-8')
     const packageLock = fs.readFileSync(`${path}/package-lock.json`, 'utf-8')
     const res = yarnToNpm(path).replace(/registry.yarnpkg.com/g, 'registry.npmjs.org')
     const resParsed = JSON.parse(res)
@@ -160,8 +119,6 @@ test('translate yarn.lock to package-lock with bundled dependencies', async t =>
     const path = `${__dirname}/fixtures/bundled-deps-yarn`
     const packageLock = fs.readFileSync(`${path}/.package-lock-snapshot.json`, 'utf-8')
     const res = yarnToNpm(path)
-    const resParsed = JSON.parse(res)
-    const pLockParsed = JSON.parse(packageLock)
     t.deepEquals(JSON.parse(res), JSON.parse(packageLock), 'result is equal to yarn.lock snapshot')
   } catch (e) {
     t.fail(e.stack)
