4a416cc4ef13e07c91051958f9eeb6d20aff0140
Aram Drevekenin <grimsniffer@gmail.com>
2018-03-17 21:32:30 +0100

    refactor: remove unused code

--- util/format.js
+++ util/format.js
@@ -2,53 +2,53 @@
 
 const ssri = require('ssri')
 
-const { findDepVersion } = require('./traverse')
+// const { findDepVersion } = require('./traverse')
 
-function flattenPackageLock (deps, flattenedTree) {
-  flattenedTree = flattenedTree || {}
-  return Object.keys(deps).reduce((flattenedTree, depName) => {
-    const { version, dependencies } = deps[depName]
-    if (!deps[depName].bundled) {
-      const resolved = deps[depName].resolved || version
-      // https://docs.npmjs.com/files/package-lock.json#version-1
-      flattenedTree[`${depName}@${version}`] = Object.assign({}, deps[depName], {resolved})
-    }
-    if (dependencies) flattenPackageLock(dependencies, flattenedTree)
-    return flattenedTree
-  }, flattenedTree)
-}
+// function flattenPackageLock (deps, flattenedTree) {
+//   flattenedTree = flattenedTree || {}
+//   return Object.keys(deps).reduce((flattenedTree, depName) => {
+//     const { version, dependencies } = deps[depName]
+//     if (!deps[depName].bundled) {
+//       const resolved = deps[depName].resolved || version
+//       // https://docs.npmjs.com/files/package-lock.json#version-1
+//       flattenedTree[`${depName}@${version}`] = Object.assign({}, deps[depName], {resolved})
+//     }
+//     if (dependencies) flattenPackageLock(dependencies, flattenedTree)
+//     return flattenedTree
+//   }, flattenedTree)
+// }
 
 module.exports = {
-  flattenPackageLock,
-  mergeDepsToYarnTree (deps, path, nodeModulesTree, tree) {
-    return Object.keys(deps).reduce((tree, dep) => {
-      const depSemver = deps[dep]
-      try {
-        const depVersion = findDepVersion(dep, nodeModulesTree, path)
-        const existingPackage = tree[dep] || {}
-        const existingPackageVersion = tree[dep] && tree[dep][depVersion]
-          ? tree[dep][depVersion]
-          : {}
-        const existingSemvers = tree[dep] &&
-          tree[dep][depVersion] &&
-          tree[dep][depVersion].semvers
-        tree[dep] = Object.assign({}, existingPackage, {
-          [depVersion]: Object.assign({},
-            existingPackageVersion,
-            {
-              semvers: existingSemvers
-                ? existingSemvers.add(depSemver)
-                : new Set([depSemver])
-            }
-          )
-        })
-        return tree
-      } catch (e) {
-        // non-existent package (eg. fsevents)
-        return tree
-      }
-    }, tree)
-  },
+//  flattenPackageLock,
+//  mergeDepsToYarnTree (deps, path, nodeModulesTree, tree) {
+//    return Object.keys(deps).reduce((tree, dep) => {
+//      const depSemver = deps[dep]
+//      try {
+//        const depVersion = findDepVersion(dep, nodeModulesTree, path)
+//        const existingPackage = tree[dep] || {}
+//        const existingPackageVersion = tree[dep] && tree[dep][depVersion]
+//          ? tree[dep][depVersion]
+//          : {}
+//        const existingSemvers = tree[dep] &&
+//          tree[dep][depVersion] &&
+//          tree[dep][depVersion].semvers
+//        tree[dep] = Object.assign({}, existingPackage, {
+//          [depVersion]: Object.assign({},
+//            existingPackageVersion,
+//            {
+//              semvers: existingSemvers
+//                ? existingSemvers.add(depSemver)
+//                : new Set([depSemver])
+//            }
+//          )
+//        })
+//        return tree
+//      } catch (e) {
+//        // non-existent package (eg. fsevents)
+//        return tree
+//      }
+//    }, tree)
+//  },
   formatYarnResolved (resolved, integrity) {
     const hexChecksum = ssri.parse(integrity).hexDigest()
     return `${resolved}#${hexChecksum}`
--- util/traverse.js
+++ util/traverse.js
@@ -1,37 +1,37 @@
 'use strict'
 
-const path = require('path')
+// const path = require('path')
 
-function parentPackagePath (parentPath) {
-  const dirs = parentPath.split(path.sep)
-  const pathDirs = dirs.slice(0, dirs.length - 1)
-  if (pathDirs[pathDirs.length - 1] === 'node_modules') {
-    return parentPackagePath(pathDirs.join(path.sep))
-  } else if (pathDirs.join(path.sep) === parentPath) {
-    throw new Error('Could not find parent dir!')
-  } else {
-    return pathDirs.join(path.sep)
-  }
-}
-
-function findDepVersion (dep, nodeModulesTree, parentPath) {
-  const depPath = path.join(parentPath, 'node_modules', dep)
-  if (nodeModulesTree[depPath]) {
-    const { version } = nodeModulesTree[depPath]
-    return version
-  } else {
-    const oneLevelDown = parentPackagePath(parentPath)
-    return findDepVersion(dep, nodeModulesTree, oneLevelDown)
-  }
-}
+// function parentPackagePath (parentPath) {
+//   const dirs = parentPath.split(path.sep)
+//   const pathDirs = dirs.slice(0, dirs.length - 1)
+//   if (pathDirs[pathDirs.length - 1] === 'node_modules') {
+//     return parentPackagePath(pathDirs.join(path.sep))
+//   } else if (pathDirs.join(path.sep) === parentPath) {
+//     throw new Error('Could not find parent dir!')
+//   } else {
+//     return pathDirs.join(path.sep)
+//   }
+// }
+//
+// function findDepVersion (dep, nodeModulesTree, parentPath) {
+//   const depPath = path.join(parentPath, 'node_modules', dep)
+//   if (nodeModulesTree[depPath]) {
+//     const { version } = nodeModulesTree[depPath]
+//     return version
+//   } else {
+//     const oneLevelDown = parentPackagePath(parentPath)
+//     return findDepVersion(dep, nodeModulesTree, oneLevelDown)
+//   }
+// }
 
 module.exports = {
-  findDepVersion,
-  findEntryInPackageLock (entry, flattenedPackageLock) {
-    const { name, version, _resolved } = entry
-    return flattenedPackageLock[`${name}@${version}`] ||
-      flattenedPackageLock[`${name}@${_resolved}`]
-  },
+//  findDepVersion,
+//  findEntryInPackageLock (entry, flattenedPackageLock) {
+//    const { name, version, _resolved } = entry
+//    return flattenedPackageLock[`${name}@${version}`] ||
+//      flattenedPackageLock[`${name}@${_resolved}`]
+//  },
   findPackageInYarnLock (name, version, yarnObject) {
     const packageKey = Object.keys(yarnObject).find(yPackage => {
       const yPackageName = yPackage.replace(/^(.+?)@.+?$/, '$1')
@@ -50,14 +50,14 @@ module.exports = {
   },
   splitPathToModules (relativePath) {
     return relativePath
-    .filter(p => p !== 'node_modules')
-    .reduce((packages, dirName, index, rPath) => {
-      if (/^@/.test(dirName)) {
-        packages.push(`${dirName}/${rPath[index + 1]}`)
-      } else if (!(rPath[index - 1] && /^@/.test(rPath[index - 1]))) {
-        packages.push(dirName)
-      }
-      return packages
-    }, [])
+      .filter(p => p !== 'node_modules')
+      .reduce((packages, dirName, index, rPath) => {
+        if (/^@/.test(dirName)) {
+          packages.push(`${dirName}/${rPath[index + 1]}`)
+        } else if (!(rPath[index - 1] && /^@/.test(rPath[index - 1]))) {
+          packages.push(dirName)
+        }
+        return packages
+      }, [])
   }
 }
