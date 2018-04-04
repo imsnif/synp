9ef03d966dbd9d5169a51f08fdd2dc155979c5de
Aram Drevekenin <grimsniffer@gmail.com>
2018-04-04 11:30:07 +0200

    refactor(packageLockTree): formatting

--- index.js
+++ index.js
@@ -4,63 +4,12 @@ const fs = require('fs')
 const path = require('path')
 const lockfile = require('@yarnpkg/lockfile')
 const eol = require('eol')
-const logicalTree = require('npm-logical-tree')
-
-const cliProgress = require('cli-progress')
-
-const jsonStringify = require('json-stable-stringify')
 
 const { createYarnTree } = require('./lib/yarn-tree')
-const { createTreeManifest } = require('./lib/tree-manifest')
-
 const { createLogicalTreeNpm, createLogicalTreeYarn } = require('./lib/logical-tree')
 const { createPhysicalTree } = require('./lib/physical-tree')
-const { packageLockTree } = require('./lib/package-lock-tree')
-
-function getTreeSize (nodeModulesTree) {
-  let counter = 0
-  nodeModulesTree.forEach((node, cb) => {
-    counter++
-    cb()
-  })
-  return counter
-}
-
-function createProgressBar (nodeModulesTree) {
-  const treeSize = getTreeSize(nodeModulesTree)
-  const bar = new cliProgress.Bar({clearOnComplete: true}, cliProgress.Presets.shades_classic)
-  bar.start(treeSize, 0)
-  return bar
-}
-
-function buildLogicalTree (packageDir) {
-  const packageJson = JSON.parse(
-    fs.readFileSync(path.join(packageDir, 'package.json'), 'utf-8')
-  )
-  const packageLock = JSON.parse(
-    fs.readFileSync(
-      path.join(packageDir, 'package-lock.json'),
-      'utf-8'
-    )
-  )
-  const nodeModulesTree = logicalTree(
-    packageJson,
-    packageLock
-  )
-  return { packageJson, packageLock, nodeModulesTree }
-}
-
-function priorityOf (keyName) {
-  if (keyName === 'name') return 1
-  if (keyName === 'version') return 2
-  if (keyName === 'resolved') return 3
-  if (keyName === 'integrity') return 4
-  if (keyName === 'bundled') return 5
-  if (keyName === 'optional') return 6
-  if (keyName === 'lockfileVersion') return 7
-  if (keyName === 'requires') return 8
-  if (keyName === 'dependencies') return 9
-}
+const { createPackageLockTree } = require('./lib/package-lock-tree')
+const { stringifyPackageLock } = require('./util/format')
 
 module.exports = {
   async yarnToNpm (packageDir) {
@@ -76,25 +25,8 @@ module.exports = {
     try {
       const logicalTree = await createLogicalTreeYarn({packageJson, yarnLock: yarnObject})
       const physicalTree = createPhysicalTree({logicalTree})
-      const packageLock = packageLockTree({physicalTree})
-      const { name, version } = packageJson
-      return jsonStringify(Object.assign({}, packageLock.toObject(), {
-        name,
-        version,
-        lockfileVersion: 1,
-        requires: true
-      }), {
-        space: 2,
-        cmp: (a, b) => {
-          const aPriority = priorityOf(a.key)
-          const bPriority = priorityOf(b.key)
-          if (aPriority && bPriority) {
-            return aPriority < bPriority ? -1 : 1
-          } else {
-            return a.key < b.key ? -1 : 1
-          }
-        }
-      })
+      const packageLock = createPackageLockTree({physicalTree})
+      return stringifyPackageLock({packageLock, packageJson})
     } catch (e) {
       throw e
     }
@@ -109,15 +41,11 @@ module.exports = {
         'utf-8'
       )
     )
-    // const bar = createProgressBar(nodeModulesTree)
     try {
       const logicalTree = await createLogicalTreeNpm({packageJson, packageLock})
       const yarnLock = createYarnTree({logicalTree})
-      // const yarnObj = yarnLock.toObject()
-      // bar.stop()
       return lockfile.stringify(yarnLock)
     } catch (e) {
-      // bar.stop()
       throw e
     }
   }
--- util/format.js
+++ util/format.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const jsonStringify = require('json-stable-stringify')
+
 module.exports = {
   formatYarnResolved (resolved, shasum) {
     if (/^git/.test(resolved)) return resolved
@@ -20,5 +22,36 @@ module.exports = {
         }
         return {deps, optDeps}
       }, {deps: {}, optDeps: {}})
+  },
+  stringifyPackageLock ({packageLock, packageJson}) {
+    const { name, version } = packageJson
+    const keyPriorities = {
+      name: 1,
+      version: 2,
+      resolved: 3,
+      integrity: 4,
+      bundled: 5,
+      optional: 6,
+      lockfileVersion: 7,
+      requires: 8,
+      dependencies: 9
+    }
+    return jsonStringify(Object.assign({}, packageLock, {
+      name,
+      version,
+      lockfileVersion: 1,
+      requires: true
+    }), {
+      space: 2,
+      cmp: (a, b) => {
+        const aPriority = keyPriorities[a.key]
+        const bPriority = keyPriorities[b.key]
+        if (aPriority && bPriority) {
+          return aPriority < bPriority ? -1 : 1
+        } else {
+          return a.key < b.key ? -1 : 1
+        }
+      }
+    })
   }
 }
