e7ae26ebc2c0d66835658c4407bd7be8a3c0bcd4
Aram Drevekenin <grimsniffer@gmail.com>
2018-04-05 13:36:54 +0200

    test(fixtures): added manifest cache

---
+++ test/fixtures/manifest-cache/index.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const nock = require('nock')
+const stubs = require('./stubs.json')
+
+const registryUrl = 'https://registry.npmjs.org' // TODO: not hard-coded
+
+Object.keys(stubs).forEach(uri => {
+  const body = JSON.stringify(stubs[uri])
+  nock(registryUrl, {allowUnmocked: true})
+  .get(uri)
+  .reply(200, body)
+  .persist()
+})
