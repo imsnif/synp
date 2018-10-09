f4ea8ab2ab6b43b9016a575c8f55be5841441645
Timur Amirov <timur.amirov@zalando.de>
2018-10-09 13:06:34 +0200

    test: failing test for git support

--- test/integration.spec.js
+++ test/integration.spec.js
@@ -343,3 +343,20 @@ test('translate yarn.lock to package-lock with tarball', async t => {
     t.end()
   }
 })
+
+test('translate yarn.lock to package-lock with git+https', async t => {
+  try {
+    t.plan(1)
+    const path = `${__dirname}/fixtures/git-deps`
+    const packageLock = fs.readFileSync(`${path}/.package-lock-snapshot.json`, 'utf-8')
+    const res = yarnToNpm(path)
+    t.deepEquals(
+      JSON.parse(res),
+      JSON.parse(packageLock),
+      'result is equal to package-lock.json snapshot'
+    )
+  } catch (e) {
+    t.fail(e.stack)
+    t.end()
+  }
+})
