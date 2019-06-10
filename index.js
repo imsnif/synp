8d22d894c8b3738bad0bb56859d8065dc9a2c784
bluelovers <codelovers@users.sourceforge.net>
2019-06-10 21:39:56 +0800

    rename

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs')
 const path = require('path')
-const { yarnToNpmCore, npmToYarnCore } = require('./lib')
+const { convertYarnToNpm, convertNpmToYarn } = require('./lib')
 
 module.exports = {
   yarnToNpm (packageDir) {
@@ -12,13 +12,13 @@ module.exports = {
     )
     const packageJson = fs.readFileSync(path.join(packageDir, 'package.json'))
     const { name, version } = JSON.parse(packageJson)
-    return yarnToNpmCore(yarnLock, name, version, packageDir)
+    return convertYarnToNpm(yarnLock, name, version, packageDir)
   },
   npmToYarn (packageDir) {
     const packageLockFileString = fs.readFileSync(
       path.join(packageDir, 'package-lock.json'),
       'utf-8'
     )
-    return npmToYarnCore(packageLockFileString, packageDir)
+    return convertNpmToYarn(packageLockFileString, packageDir)
   }
 }
